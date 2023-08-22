753b0cbbfcdff7aedb27228269349ea24572689c
Massimiliano Porzio <massimiliano.porzio@gmail.com>
2023-08-22 17:53:46 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flame_audio/flame_audio.dart';
+import 'package:flutter/material.dart';
+import 'package:supermario_flutter/constants/sprite_sheets.dart';
+
+import 'constants/globals.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  //load sprites
+  await SpriteSheets.load();
+  await FlameAudio.audioCache.loadAll([
+    Globals.jumpSmallSFX,
+    Globals.pauseSFX,
+    Globals.bumpSFX,
+    Globals.powerUpAppearsSFX,
+    Globals.breakBlockSFX,
+  ]);
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        home: Center(
+          child: Text('Super Mario Bros.'),
+        ));
+  }
+}
